750415da36c0ea9cad1cd56716c6741b02fa5e7a
Ian Zaragoza <ianzaragoza@msn.com>
2018-09-11 18:50:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens = kittens.slice(0, kittens.length -1);
-  return kittens
+  return kittens.slice(0, kittens.length -1);
+
 }
